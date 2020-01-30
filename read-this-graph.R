6499209d6aad7eb0b0f425292b5571cb7ac3f026
Strochak <sstrochak@urban.org>
2020-01-30 16:13:45 -0500

    update with Q4 homeownership numbers

--- read-this-graph.R
+++ read-this-graph.R
@@ -4,7 +4,7 @@ library(lubridate)
 
 set_urbn_defaults(style = "print")
 
-cps <- read_csv("data/cps-horate_2000_2019-07-01.csv")
+cps <- read_csv("data/cps-horate_2000_2019-10-01.csv")
 
 sample <- cps %>% 
   filter(year >= 2015) %>% 
@@ -20,7 +20,7 @@ howtoread <- ggplot(data = sample, mapping = aes(date, horate)) +
               alpha = 0.23,
               fill = "#6f6f6f") +
   annotate(geom = "rect",
-           xmin = ymd("2019-07-01"), xmax = ymd("2020-06-01"),
+           xmin = ymd("2019-10-01"), xmax = ymd("2020-09-01"),
            ymin = 0.619, ymax = 0.66,
            fill = "white", color = NA) +
   geom_ribbon(aes(ymin = horate_low, ymax = horate_high, fill = region),
@@ -35,12 +35,12 @@ howtoread <- ggplot(data = sample, mapping = aes(date, horate)) +
   guides(color = guide_legend(override.aes = list(fill = NA))) +
   geom_line(data = sample, mapping = aes(date, horatemax_line),
             linetype = 2, color = "#6f6f6f", alpha = 0.5) +
-  annotate(geom = "text", x = ymd("2016-05-01"), y = mean(sample$horatemax_line) + 0.0015,
+  annotate(geom = "text", x = ymd("2016-04-01"), y = mean(sample$horatemax_line) + 0.0015,
            label = "90% confidence interval upper bound",
            color = "#6f6f6f")+
   geom_line(data = sample, mapping = aes(date, horatemin_line),
             linetype = 2, color = "#6f6f6f", alpha = 0.5) +
-  annotate(geom = "text", x = ymd("2016-12-01"), y = mean(sample$horatemin_line) - 0.0015,
+  annotate(geom = "text", x = ymd("2016-04-01"), y = mean(sample$horatemin_line) - 0.0015,
            label = "90% confidence interval lower bound",
            color = "#6f6f6f") +
   geom_point(data = filter(sample, date == max(sample$date)),
@@ -49,9 +49,12 @@ howtoread <- ggplot(data = sample, mapping = aes(date, horate)) +
   geom_point(data = filter(sample, date == max(sample$date)),
              mapping = aes(x = date, y = horatemin_line),
              color = "black", alpha = 0.5, size = 2) +
-  annotate(geom = "text", x = ymd("2019-07-14"),
+  geom_point(data = filter(sample, date == max(sample$date)),
+             mapping = aes(x = date, y = horate),
+             color = "#1696d2", size = 2) +
+  annotate(geom = "text", x = ymd("2019-10-14"),
            y = rate,
-           label = "The value for\nthe 3rd quarter of\n 2019 is in this \n range",
+           label = "The value for\nthe 4th quarter of\n 2019 is in this \n range",
            size = 3,
            hjust = 0) +
   scale_y_continuous(labels = scales::percent_format(accuracy = 1),
@@ -63,7 +66,7 @@ howtoread <- ggplot(data = sample, mapping = aes(date, horate)) +
                           ymd("2019-01-01")),
                labels = scales::date_format("%Y"),
                expand = c(0, 0),
-               limits = c(ymd("2015-01-01"), ymd("2020-06-01"))) +
+               limits = c(ymd("2015-01-01"), ymd("2020-09-01"))) +
   labs(x = NULL, y = NULL)
 
 p <- urbn_plot(urbn_title("How To Read This Graph"),
