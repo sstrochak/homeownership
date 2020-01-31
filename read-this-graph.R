957992a08f9623477a480d478c73748da5d7bdca
Strochak <sstrochak@urban.org>
2020-01-31 15:32:21 -0500

    make copy edits to figures for blog post

--- read-this-graph.R
+++ read-this-graph.R
@@ -35,14 +35,16 @@ howtoread <- ggplot(data = sample, mapping = aes(date, horate)) +
   guides(color = guide_legend(override.aes = list(fill = NA))) +
   geom_line(data = sample, mapping = aes(date, horatemax_line),
             linetype = 2, color = "#6f6f6f", alpha = 0.5) +
-  annotate(geom = "text", x = ymd("2016-04-01"), y = mean(sample$horatemax_line) + 0.0015,
-           label = "90% confidence interval upper bound",
-           color = "#6f6f6f")+
+  annotate(geom = "text", x = ymd("2016-04-15"), y = mean(sample$horatemax_line) + 0.0015,
+           label = "Upper bound confidence interval for Q4 2019",
+           color = "#6f6f6f",
+           size = 3)+
   geom_line(data = sample, mapping = aes(date, horatemin_line),
             linetype = 2, color = "#6f6f6f", alpha = 0.5) +
-  annotate(geom = "text", x = ymd("2016-04-01"), y = mean(sample$horatemin_line) - 0.0015,
-           label = "90% confidence interval lower bound",
-           color = "#6f6f6f") +
+  annotate(geom = "text", x = ymd("2016-04-15"), y = mean(sample$horatemin_line) - 0.0015,
+           label = "Lower bound confidence interval for Q4 2019",
+           color = "#6f6f6f",
+           size = 3) +
   geom_point(data = filter(sample, date == max(sample$date)),
              mapping = aes(x = date, y = horatemax_line),
              color = "black", alpha = 0.5, size = 2) +
@@ -69,13 +71,14 @@ howtoread <- ggplot(data = sample, mapping = aes(date, horate)) +
                limits = c(ymd("2015-01-01"), ymd("2020-09-01"))) +
   labs(x = NULL, y = NULL)
 
-p <- urbn_plot(urbn_title("How To Read This Graph"),
+p <- urbn_plot(urbn_title("US Homeownership Rate, Housing Vacancy Survey"),
           get_legend(howtoread),
           urbn_y_title("Homeownership rate"),
           remove_legend(howtoread),
-          urbn_source("Current Population Survey/Housing Vacancy Survey"),
+          urbn_source("US Census Bureau's Housing Vacancy Survey."),
+          urbn_note("Data for Housing Vacancy Survey are quarterly."),
           urbn_logo_text(),
-          heights = c(0.1, 0.05, 0.05, 1, 0.05, 0.05))
+          heights = c(0.1, 0.05, 0.05, 1, 0.035, 0.035, 0.035))
 
 ggsave("plots/read-this-graph.png",
        plot = p,
